961a2ecc13fa74ec4b84cf6d351dbf2085fc533b
alishavirani0317 <avirani3@illinois.edu>
2025-07-01 09:31:46 -0500

    Delete TasteMatch directory

--- TasteMatch/backend/routes/restaurantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const restaurantController = require("../controllers/restaurantController");
-
-router.post("/restaurants", restaurantController.createRestaurant);
-router.get("/restaurants", restaurantController.getAllRestaurants);
-
-module.exports = router;
--- TasteMatch/backend/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reviewController = require("../controllers/reviewController");
-
-router.post("/reviews", reviewController.createReview);
-router.get("/reviews/:restaurantId", reviewController.getReviewsForRestaurant);
-
-module.exports = router;
