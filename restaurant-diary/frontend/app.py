59c323540d95ca69e14256bcc59d8a430d3c11fc
avirani1204 <avirani3@illinois.edu>
2025-07-14 13:46:01 -0500

    comments

--- restaurant-diary/frontend/app.py
+++ restaurant-diary/frontend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
+# instance of flask object
 app = Flask (__name__)
 
+#map url route to this function
 @app.route("/")
 
 def home():
-    return "Hello World, from Flask!"
+    return "Welcome to Restaurant Diary!"
