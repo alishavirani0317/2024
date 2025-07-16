7f2afa4603c47b2d634f8c71f5745b975779cda1
avirani1204 <avirani3@illinois.edu>
2025-07-16 12:09:15 -0500

    setup flask sql alchemy

--- restaurant-diary/website/__init__.py
+++ restaurant-diary/website/__init__.py
@@ -1,6 +1,7 @@
 # how u initialize flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy() # define new databse
 DB_NAME = "databse.db"
@@ -19,4 +20,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # create db
+    from .models import User, Note 
+
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    # check if db exists (if not we will create it)
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
