dd5785cd5624c0c9c55f81a3e2ac58b6816b948e
avirani1204 <avirani3@illinois.edu>
2025-07-16 10:31:21 -0500

    added flash messages to auth.py and trying to show them in base.html

--- restaurant-diary/website/auth.py
+++ restaurant-diary/website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 # want to make sure login and signup r able to accept post requests
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 
 def login():
     return render_template("login.html", boolean=True)
@@ -12,7 +12,30 @@ def login():
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            #alert user if something went wrong (message flashing (import flash))
+            flash('Email must be greater than 4 characters.', category='error')
+
+        
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            #add user to database
+            flash ('Account created!', category='success')
+
+
     return render_template("signup.html")
 
