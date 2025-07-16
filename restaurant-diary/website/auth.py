aa0b1479e700016a6f908abc45b540cfa5c566f3
avirani1204 <avirani3@illinois.edu>
2025-07-16 10:09:19 -0500

    want to make sure login and signup r able to accept post requests (methods=[GET, POST])

--- restaurant-diary/website/auth.py
+++ restaurant-diary/website/auth.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# want to make sure login and signup r able to accept post requests
+@auth.route('/login', methods=['GET', 'POST'])
+
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,7 +12,7 @@ def login():
 def logout():
     return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("signup.html")
 
