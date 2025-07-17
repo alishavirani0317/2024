faa54832249597ff3117952ad33a83d24c053dfd
avirani1204 <avirani3@illinois.edu>
2025-07-17 10:05:27 -0500

    tried implementing react frontend never worked

--- restaurant-diary/website/models.py
+++ restaurant-diary/website/models.py
@@ -3,15 +3,12 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class RestaurantEntry(db.Model):
+class Note(db.Model):
     #all notes need to look like this
     id = db.Column(db.Integer, primary_key=True)
-    restaurant_name = db.Column(db.String(255), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    notes = db.Column(db.String(10000))
-    visit_date = db.Column(db.DateTime, default=func.now())
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default= func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 class User(db.Model, UserMixin):
     #all users need to look like this
@@ -20,8 +17,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True) # no email can have same email as other user
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    entries = db.relationship('RestuarantEntry') # everytime we can make a note, add into this users' notes relationship that notes id
-
-# flask shell
-# >>> from website import db
-# >>> db.create_all() 
+    notes = db.relationship('Note') # everytime we can make a note, add into this users' notes relationship that notes id
