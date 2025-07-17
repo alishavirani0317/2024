3c0fc47fa27020ad4bb2aa1f302ad6ea1d840342
avirani1204 <avirani3@illinois.edu>
2025-07-17 13:46:14 -0500

    started movie card component

--- movie-favorites/frontend/src/App.jsx
+++ movie-favorites/frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import MovieCard from './components /MovieCard';
+//where react stuff goes 
 
+//component start with capital letter
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    //return just one root element per level
+    // fragment -> empty tag <>
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <MovieCard movie={{title: "Alisha's Film", release_date:"2024"}}/>
+      <MovieCard movie={{title: "Dih's Film", release_date:"2024"}}/>
 
-export default App
+    </>
+  );
+}
+// add prop : set of curly braces
+
+
+
+export default App;
