3f7e6df6291c96eb50b55624119048a975c37de9
avirani1204 <avirani3@illinois.edu>
2025-07-17 09:28:41 -0500

    changed models.py to reflect restaurant entries

--- restaurant-diary/website/models.py
+++ restaurant-diary/website/models.py
@@ -3,12 +3,15 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
+class RestaurantEntry(db.Model):
     #all notes need to look like this
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default= func.now())
+    restaurant_name = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.String(10000))
+    visit_date = db.Column(db.DateTime, default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class User(db.Model, UserMixin):
     #all users need to look like this
@@ -17,4 +20,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True) # no email can have same email as other user
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note') # everytime we can make a note, add into this users' notes relationship that notes id
+    entries = db.relationship('RestuarantEntry') # everytime we can make a note, add into this users' notes relationship that notes id
